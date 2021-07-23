0db62b66a926be164ab0d8946ee8126c2ad85e75
Nishu Tyagi <tyaginishu7@gmail.com>
2021-07-23 13:24:10 +0530

    Added:Lengths of Two Lines

--- LineLengths.java
+++ LineLengths.java
@@ -1,5 +1,12 @@
-public class Welcome {
-    public static void main (String[], args); {
+import java.lang.Math;
+public class LineLengthsUC1 {
+    public static void main (String arg[]) {
         System.out.println("Welcome to Line Comparison Computation Program");
-    }
+        
+        int x1,x2,y1,y2;
+        double Length;
+	    x1=2;y1=2;x2=4;y2=4;
+	    Length=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("Distance Between Two Lines" +Length);
+	}
 }
