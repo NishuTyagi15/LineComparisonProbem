8bb8cca348426c42a074f7fad17356c9450ae934
Nishu Tyagi <tyaginishu7@gmail.com>
2021-07-24 19:12:43 +0530

    Refactored:Check Wheather two Lines are Equal or Not

--- LineLengths.java
+++ LineLengths.java
@@ -1,12 +1,24 @@
-import java.lang.Math;
-public class LineLengthsUC1 {
-    public static void main (String arg[]) {
+public class LineLengths {
+    public static void main (String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
         
+        //UC1
         int x1,x2,y1,y2;
-        double Length;
 	    x1=2;y1=2;x2=4;y2=4;
-	    Length=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        System.out.println("Distance Between Two Lines" +Length);
+	    double Length1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("Length of Line 1:" +Length1);
+
+        //UC2
+        int p1, p2, q1, q2;
+	    p1=1;q1=1;p2=4;q2=4;
+	    double Length2=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("Length of Line 2:" +Length2);
+
+        if (Length1 == Length2){
+            System.out.println("Both Lines are Equal");
+        } 
+        else {
+            System.out.println("Lines are not Equal");
+        }        
 	}
 }
