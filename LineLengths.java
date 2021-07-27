d35b924fee0e433968d8779edadf30529623c2c2
Nishu <87470273+NishuTyagi15@users.noreply.github.com>
2021-07-27 15:24:30 +0530

    Refactored:Use Power istead of Multiplying

--- LineLengths.java
+++ LineLengths.java
@@ -1,12 +1,12 @@
 import java.lang.Math;
-public class LineLengthsUC1 {
+public class lineLengthsUC1 {
     public static void main (String arg[]) {
         System.out.println("Welcome to Line Comparison Computation Program");
         
         int x1,x2,y1,y2;
         double Length;
 	    x1=2;y1=2;x2=4;y2=4;
-	    Length=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+	    Length=Math.sqrt((x2-x1)^2 + (y2-y1)^2);
         System.out.println("Distance Between Two Lines" +Length);
 	}
 }
