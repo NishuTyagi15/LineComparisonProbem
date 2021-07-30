dbcabc7c62ac6c363870346cd8a2e2eade09930c
Nishu Tyagi <tyaginishu7@gmail.com>
2021-07-30 18:08:28 +0530

    Refactored:Line Comparison Using OOPS Concepts

--- LineLengths.java
+++ LineLengths.java
@@ -1,24 +1,28 @@
 import java.lang.Math;
 import java.lang.Double;
 
-public class lineLengths {
-    public static void main (String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
-        
-        //UC1
+public class LineLengths {
+    public static double lineLengthCal(int x1, int x2, int y1, int y2) {
+        double length=Math.ceil(Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2)));
+		return length;
+    }
+    public static void lineEqualsComp() {
         int x1,x2,y1,y2;
 	    x1=2;y1=2;x2=4;y2=4;
-	    double length1=Math.ceil(Math.sqrt((x2-x1)^2 + (y2-y1)^2));
-        System.out.println("Length of Line 1:" +length1);
 
-        //UC2
+        Double length1 = lineLengthCal(x1,x2,y1,y2);
+		
+		String l1=String.valueOf(length1); 
+        System.out.println("Length of Line 1:" +l1);
+
         int p1, p2, q1, q2;
 	    p1=1;q1=1;p2=4;q2=4;
-	    double length2=Math.ceil(Math.sqrt((p2-p1)^2 + (q2-q1)^2));
-        System.out.println("Length of Line 2:" +length2);
 
-        String l1=String.valueOf(length1);  
-	    String l2=String.valueOf(length2); 
+        Double length2 = lineLengthCal(p1,p2,q1,q2);
+			
+		
+		String l2=String.valueOf(length2);  
+        System.out.println("Length of Line 2:" +l2);
 
         if (l1.equals(l2)){
             System.out.println("Both Lines are Equal");
@@ -26,8 +30,7 @@ public class lineLengths {
         else {
             System.out.println("Lines are not Equal");
         }
-        
-        //UC3
+
         Double x = length1;
         Double y = length2;
 
@@ -42,5 +45,10 @@ public class lineLengths {
         else {
             System.out.println("Length1 is Greater than Length2");
         }
+    }
+    public static void main(String[] args) {
+		
+        lineEqualsComp();
+        
 	}
 }
